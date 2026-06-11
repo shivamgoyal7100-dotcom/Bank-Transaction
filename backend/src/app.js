5c138733241491c0e24be153c29d4cd7d079c187
shivamgoyal7100-dotcom <shivamgoyal7100@gmail.com>
2026-06-11 23:13:29 +0530

    add health check route to confirm service status

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,11 @@ const authRouter = require("./routes/auth.routes")
 const accountRouter = require("./routes/account.routes")
 const transactionRouter = require("./routes/transaction.route")
 
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the banking API")
+}
+
 app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter)
 app.use("/api/transaction", transactionRouter)
